da5092e90d0b5055b521ed31cf2117f00bd8005a
Bharat Panwar <55885130+bsingh23@users.noreply.github.com>
2021-03-07 15:14:47 +0530

    added nor_inv.py and ip_addr file

---
+++ nor_inv.py
@@ -0,0 +1,17 @@
+def nor_inv(filename): 
+    with open(filename) as f: 
+        data = f.read().splitlines() 
+        newlist = [] 
+        for values in data: 
+            name_ip = tuple(values.split(" ")) 
+            newlist.append(name_ip) 
+        for name,ip in newlist: 
+            print(f'{name.upper()}:') 
+            print(f"   hostname: {ip}") 
+            print("   group") 
+            site_abb = name.split("-") 
+            print(f"       - {site_abb[0].upper()}")
+            print("   platform: 'ios'")
+
+if __name__ == '__main__':
+  nor_inv("ip_addr.txt")
